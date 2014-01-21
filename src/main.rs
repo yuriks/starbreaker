c8e6e0b63602b3634ee6f2be27097e2b39195110
Yuri Kunde Schlesner <yuriks@yuriks.net>
2014-01-21 05:55:09 -0200

    Fix print() handling of non-ASCII chars.

--- src/main.rs
+++ src/main.rs
@@ -19,21 +19,23 @@ fn print(s: &str) {
 	static buf_len: uint = 4096;
 	let mut buffer = [0u16, ..buf_len];
 
-	let mut i = 0;
-	while i < buf_len - 1 && i < str_len(s) {
-		buffer[i] =
-			if (s[i] & 0x80 == 0) { // ASCII character
-				s[i] as u16
-			} else { // Non-ASCII character
-				if (s[i] & 0x40 != 0) { // Leading byte
-					'?' as u16
-				} else { // Continuation, don't output anything
-					continue;
-				}
-			};
-		i += 1;
+	let mut src_i = 0;
+	let mut dst_i = 0;
+	while dst_i < buf_len - 1 && src_i < str_len(s) {
+		if (s[src_i] & 0x80 == 0) { // ASCII character
+			buffer[dst_i] =	s[src_i] as u16;
+		} else { // Non-ASCII character
+			if (s[src_i] & 0x40 != 0) { // Leading byte
+				buffer[dst_i] = '?' as u16;
+			} else { // Continuation, don't output anything
+				src_i += 1;
+				continue;
+			}
+		}
+		src_i += 1;
+		dst_i += 1;
 	}
-	buffer[i] = 0u16;
+	buffer[dst_i] = 0u16;
 
 	unsafe {
 		print_utf16(core::mem::transmute(&buffer));
@@ -45,7 +47,7 @@ fn print(s: &str) {
 pub extern "win64" fn efi_main(image_handle: efi::HANDLE, system_table_: *efi::SYSTEM_TABLE) -> efi::STATUS {
 	unsafe {
 		system_table = system_table_;
-		print("Hello World!\r\n");
+		print("Hello ß World!\r\n");
 
 		loop { }
 	}
